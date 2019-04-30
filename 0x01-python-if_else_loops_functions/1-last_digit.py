63a02e1597c1964b469f48ef303440daa42ddd88
klupzor <lopezgand@gmail.com>
2019-04-30 22:12:59 +0000

    fixing text

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ d = abs(number) % 10
 if number < 0:
     d = d * -1
 if d > 5:
-    print("Las digit of {:d} is {:d} and is grater than 5".format(number, d))
+    print("Last digit of {:d} is {:d} and is grater than 5".format(number, d))
 elif d == 0:
-    print("Las digit of {:d} is {:d} and is 0".format(number, d))
+    print("Last digit of {:d} is {:d} and is 0".format(number, d))
 else:
-    print("Las digit of {:d} is {:d} and is less than 6 and not 0"
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
           .format(number, d))
