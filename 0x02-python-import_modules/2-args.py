aa270c051bd147f7be6aa228a476cc1971eb2608
klupzor <lopezgand@gmail.com>
2019-05-03 21:58:09 +0000

    fixing format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     args = len(sys.argv) - 1
     if args == 0:
         print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(args))
         for c in range(1, args + 1):
