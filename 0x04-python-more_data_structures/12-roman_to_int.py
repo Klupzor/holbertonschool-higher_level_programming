b207506e8576520a4f33068a60015a058a2a5761
klupzor <lopezgand@gmail.com>
2019-05-10 02:12:26 +0000

    adding none condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    else:
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         n = list(roman_string)
         nums = list(map(lambda key: rom[key], n))
