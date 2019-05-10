1d639a2e15f9e731d367f053b08293b479d5a566
klupzor <lopezgand@gmail.com>
2019-05-10 01:25:52 +0000

    adding empty condition

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return (0)
     num = sum(list(map(lambda t: t[0] * t[1], my_list)))
     den = sum(list(map(lambda t: t[1], my_list)))
     return num / den
