c089ff949ae71fac3a2bac483586920ba98cdfab
klupzor <lopezgand@gmail.com>
2019-05-21 22:28:31 +0000

    adding conditions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,16 +21,21 @@ class Square:
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
-        self.__size = value
+        else:
+            self.__size = value
         if value < 0:
             raise ValueError("size must be >= 0")
-        return self.__size
+        else:
+            return self.__size
 
     @position.setter
     def position(self, value):
-        if type(value[0]) != int or type(value[1]) != int:
+        if (type(value) != tuple or len(value) != 2 or
+                type(value[0]) != int or type(value[1]) != int or
+                value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def my_print(self):
         if self.size == 0:
