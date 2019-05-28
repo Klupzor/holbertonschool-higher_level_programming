635b1d2e99b39f7828f1e04a80a369e55d9a6d05
klupzor <lopezgand@gmail.com>
2019-05-28 02:38:30 +0000

    creating testing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Real definition of a rectangle 
+"""Real definition of a rectangle
 """
 
 
@@ -10,8 +10,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -25,9 +25,10 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
