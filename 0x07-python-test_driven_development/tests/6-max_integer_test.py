4d489cbbfd9aba9a070265ef5542d1fc1dd7c280
klupzor <lopezgand@gmail.com>
2019-05-29 16:08:10 +0000

    adding case to test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,6 +7,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4]), 4)
         self.assertEqual(max_integer([1, 7, 3, 4]), 7)
         self.assertEqual(max_integer([-1, -7, -3, -4]), -1)
         self.assertEqual(max_integer([-1, -7, 9.3, -4]), 9.3)
