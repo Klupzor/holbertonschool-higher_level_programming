0003f055c36f9ebd89d46ede9ed18598855c6dea
klupzor <lopezgand@gmail.com>
2019-05-29 16:22:06 +0000

    changing funtion to matmul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -27,44 +27,4 @@ def lazy_matrix_mul(m_a, m_b):
     matrix
         A new matrix
     """
-    ms = ["m_a should contain only integers or floats",
-          "m_b should contain only integers or floats",
-          "each row of m_a must should be of the same size",
-          "each row of m_b must should be of the same size"]
-    if type(m_a) != list:
-        raise TypeError("m_a must be a list")
-
-    if type(m_b) != list:
-        raise TypeError("m_b must be a list")
-
-    if len(m_a) == 0:
-        raise TypeError("m_a can't be empty")
-
-    for ali in m_a:
-        if type(ali) != list:
-            raise TypeError("m_a must be a list of lists")
-        if len(ali) == 0:
-            raise TypeError("m_a can't be empty")
-        for n in ali:
-            if not type(n) == int or type(n) == float:
-                raise TypeError(ms[0])
-        if not len(ali) == len(m_a[0]):
-            raise TypeError(ms[2])
-
-    if len(m_b) == 0:
-        raise TypeError("m_b can't be empty")
-    for bli in m_b:
-        if type(bli) != list:
-            raise TypeError("m_b must be a list of lists")
-        if len(bli) == 0 or len(m_b) == 0:
-            raise TypeError("m_b can't be empty")
-        for n in bli:
-            if not type(n) == int or type(n) == float:
-                raise TypeError(ms[1])
-        if not len(bli) == len(m_b[0]):
-            raise TypeError(ms[3])
-
-    if not len(m_a[0]) == len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
-    return npy.dot(m_a, m_b)
+    return npy.matmul(m_a, m_b)
