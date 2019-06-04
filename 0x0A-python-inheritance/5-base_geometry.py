a2b13073909c77d47476988684f80514cbd0ddc0
klupzor <lopezgand@gmail.com>
2019-06-04 22:04:32 +0000

    finish 9task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Geometry module
+    Empty class
+'''
+
+
+class BaseGeometry:
+    '''
+        Empty class
+    '''
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Improve Geometry
+'''
+
+
+class BaseGeometry:
+    '''Geometry module
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
