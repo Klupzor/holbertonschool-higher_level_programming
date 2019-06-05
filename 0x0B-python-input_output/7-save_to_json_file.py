6b12d5ceb21b55b612023269b26352838a94c023
klupzor <lopezgand@gmail.com>
2019-06-05 22:06:09 +0000

    delating return

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     text = json.dumps(my_obj)
     with open(filename, 'w', encoding="UTF8") as f:
-        return f.write(text)
+        f.write(text)
