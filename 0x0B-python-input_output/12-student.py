d2289451b19261797d4e5df5f9b226ff8e7be279
klupzor <lopezgand@gmail.com>
2019-06-06 01:17:02 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Student to JSON with filter
+'''
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs == None:
+            ndict = {}
+            for key in attrs:
+                if key in self.__dict__.keys():
+                    ndict[key] = self.__dict__[key]
+            return ndict
+        else:
+            return self.__dict__
