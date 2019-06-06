bde5bd32b1b79942b7705def9b0192d76b99dd78
Andres <lopezgand@gmail.com>
2019-06-06 04:41:48 +0000

    adding case

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -11,26 +11,19 @@ def pascal_triangle(n):
     triangle.append(li)
     if n == 1:
         return triangle;
-    li = [1, 1]
-    triangle.append(li)
-    if n == 2:
-        return triangle;
-
-    for i in range(n - 2):
-        for tr in triangle:
-            size = len(tr)
-            if size >= 2:
-                li = [1]
-                for cont in range(size):
-                    print(cont)
-                    if  cont == size - 1:
-                        li.append(1)
-                        break
-                    else:
-                        li.append(tr[cont] + tr[cont + 1])
-                    print(li)
-                triangle.append(li)
-    return triangle;
+    i = 0
+    for tr in triangle:
+        size = len(tr)
+        li = [1]
+        for cont in range(size):
+            if  cont >= size - 1:
+                li.append(1)
+            else:
+                li.append(tr[cont] + tr[cont + 1])
+        triangle.append(li)
+        i += 1
+        if i == n - 1:
+            return triangle
             
 
-print(pascal_triangle(3))
+print(pascal_triangle(5))
