0d33739d66039a1d59bbe072000688d234ec5ce0
klupzor <lopezgand@gmail.com>
2019-06-07 20:41:05 +0000

    finish 2 task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Base class
+'''
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if not id:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
