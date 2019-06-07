65bda8dd688bcb64fa43b4d5cd32f7df50ad347f
klupzor <lopezgand@gmail.com>
2019-06-07 21:20:10 +0000

    finishing task3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''First Rectangle
+'''Rectangle
 '''
 from models.base import Base
 
@@ -11,29 +11,49 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
-        
+
+    def __validate_input(self, name, value):
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if name == "width" or name == "height":
+            if value <= 0:
+                raise ValueError(name + " must be > 0")
+        if name == "x" or name == "y":
+            if value < 0:
+                raise ValueError(name + " must be >= 0")
+
     @property
     def width(self):
         return self.__width
+
     @property
     def height(self):
         return self.height
+
     @property
     def x(self):
         return self.x
+
     @property
     def y(self):
         return self.y
 
     @width.setter
     def width(self, value):
+        self.__validate_input("width", value)
         self.__width = value
+
     @height.setter
     def height(self, value):
+        self.__validate_input("height", value)
         self.__height = value
+
     @x.setter
     def x(self, value):
+        self.__validate_input("x", value)
         self.__x = value
+
     @y.setter
     def y(self, value):
+        self.__validate_input("y", value)
         self.__y = value
