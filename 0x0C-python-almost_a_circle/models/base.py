0c2044f256b83665447f23ddc8c7268ab6f612de
klupzor <lopezgand@gmail.com>
2019-06-11 20:04:20 +0000

    adding empty case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''convert to json'''
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return []
         return json.dumps(list_dictionaries)
 
     @classmethod
