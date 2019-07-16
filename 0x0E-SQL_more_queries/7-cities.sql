376e3e0bf8fdfacc7ad01136bcbe9334c531aeb1
klupzor <lopezgand@gmail.com>
2019-07-16 20:36:13 +0000

    adding primary key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- creates the database hbtn_0d_usa and foreing key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT NOT NULL AUTO_INCREMENT NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES states(id),
+	PRIMARY KEY(id)
 	);
