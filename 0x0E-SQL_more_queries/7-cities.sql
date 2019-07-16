6bd63628c60e0c659d7a2626ed7e8d22549aa983
klupzor <lopezgand@gmail.com>
2019-07-16 20:29:48 +0000

    script that creates the database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- creates the database hbtn_0d_usa and foreing key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
