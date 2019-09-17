1ec9de7a36be717218f1e313d82d81a9591aca87
klupzor <lopezgand@gmail.com>
2019-09-17 21:18:25 +0000

    fixing number

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 if (process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Not a number');
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
