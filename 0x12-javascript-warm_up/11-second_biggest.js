365bee2f4d4477bab195a6756037fb9e0ee8d7e2
klupzor <lopezgand@gmail.com>
2019-09-17 22:20:01 +0000

    show the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const nums = [];
+for (let i = 2; i < process.argv.length; i++) {
+  nums.push(parseInt(process.argv[i]));
+}
+nums.sort();
+if (nums.length < 2) { console.log(0); } else { console.log(nums[nums.length - 2]); }
