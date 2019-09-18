03cf193f49e7038cf94ec7592e7008095d7459e6
klupzor <lopezgand@gmail.com>
2019-09-18 01:56:24 +0000

    adding params to sort function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ const nums = [];
 for (let i = 2; i < process.argv.length; i++) {
   nums.push(parseInt(process.argv[i]));
 }
-nums.sort();
-if (nums.length < 2) { console.log('0'); } else { console.log(nums[nums.length - 2].toString()); }
+nums.sort((a, b) => b - a);
+if (nums.length < 2) { console.log('0'); } else { console.log(nums[1].toString()); }
