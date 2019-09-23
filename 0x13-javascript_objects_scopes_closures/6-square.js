363261abbe7542897bd4186b4fadd0698075f402
klupzor <lopezgand@gmail.com>
2019-09-23 12:11:52 -0500

    Adding printc method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const SquareF = require('./5-square');
+
+module.exports = class Square extends SquareF {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
