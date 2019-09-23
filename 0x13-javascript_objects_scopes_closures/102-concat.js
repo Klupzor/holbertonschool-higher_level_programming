6c288535df5c80ac8cd4da1748409e0a83c3ba4a
klupzor <lopezgand@gmail.com>
2019-09-23 16:36:16 -0500

    concat 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+var fs = require('fs');
+const args = process.argv;
+
+const data1 = fs.readFileSync(args[2]).toString();
+const data2 = fs.readFileSync(args[3]).toString();
+const data = data1 + data2;
+fs.writeFile(args[4], data, (err) => {
+  if (err) console.log(err);
+});
