2c742fb641d80e825d46ae2b31597fdb201c4973
klupzor <lopezgand@gmail.com>
2019-09-23 12:20:45 -0500

    adding type

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ module.exports = class Square extends SquareF {
   }
 
   charPrint (c) {
-    if (c) {
+    if (typeof c === 'string') {
       for (let i = 0; i < this.size; i++) {
         console.log(c.repeat(this.size));
       }
