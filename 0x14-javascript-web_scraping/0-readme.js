0e558f59ef82baae7d41135f77793c53128c80b8
klupzor <lopezgand@gmail.com>
2019-09-24 21:26:37 -0500

    read file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const args = process.argv;
+
+fs.readFile(args[2], 'utf8', function (err, data) {
+  if (err) console.log(err);
+  else { console.log(data.toString()); }
+});
