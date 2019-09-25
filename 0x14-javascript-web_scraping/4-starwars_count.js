5f3c25f5fbf5d9c4260dc7ab3c72cffb741f536d
klupzor <lopezgand@gmail.com>
2019-09-24 23:15:50 -0500

    adding includes method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request(url, function (error, response, body) {
   if (error) { console.log(error); } else {
     const characters = JSON.parse(body).results.map((data) => data.characters);
     for (let i = 0; i < characters.length; i++) {
-      cont = cont + characters[i].filter((x) => x === 'https://swapi.co/api/people/18/').length;
+      cont = cont + characters[i].filter((x) => x.includes('18')).length;
     }
 
     console.log(cont);
