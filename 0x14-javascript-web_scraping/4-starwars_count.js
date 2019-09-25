388e4a4b120cb381759655a1384d43327fa25a6d
klupzor <lopezgand@gmail.com>
2019-09-24 23:08:12 -0500

    count character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv;
+const request = require('request');
+let cont = 0;
+
+const url = args[2];
+
+request(url, function (error, response, body) {
+  if (error) { console.log(error); } else {
+    const characters = JSON.parse(body).results.map((data) => data.characters);
+    for (let i = 0; i < characters.length; i++) {
+      cont = cont + characters[i].filter((x) => x === 'https://swapi.co/api/people/18/').length;
+    }
+
+    console.log(cont);
+  }
+});
